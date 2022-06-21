f2cf1046069cce1b7e9502ab11c13ce7ad12f10e
Ian Shen <ian891104@berkeley.edu>
2022-06-21 10:17:37 +0800

    Initial Commit

---
+++ ExampleSQLite/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExampleSQLite
+//
+//  Created by Ian Shen on 6/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
