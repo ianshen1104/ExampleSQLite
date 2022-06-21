3df97152efcf3af240b6f370e8513bd19850c628
Ian Shen <ian891104@berkeley.edu>
2022-06-21 10:25:13 +0800

    Added sqlite3 library and header file for linkage

---
+++ ExampleSQLite/BridgeHeader.h
@@ -0,0 +1,13 @@
+//
+//  BridgeHeader.h
+//  ExampleSQLite
+//
+//  Created by Ian Shen on 6/21/22.
+//
+
+#ifndef BridgeHeader_h
+#define BridgeHeader_h
+
+#include "sqlite3.h"
+
+#endif /* BridgeHeader_h */
