6b2c809e6b3db1c6bd21c2b3e98960ad0a1ccd9f
Ian Shen <ian891104@berkeley.edu>
2022-06-21 11:33:39 +0800

    Tested insert, update, delete in viewDidLoad

--- ExampleSQLite/ViewController.swift
+++ ExampleSQLite/ViewController.swift
@@ -27,12 +27,55 @@ class ViewController: UIViewController {
 
         db = SQLiteConnector(path: sqlitePath)
 
-        if let mydb = db { //Try playing with the db
-            let _ = mydb.createTable("people", columnsInfo: [
+        //Try playing with the db
+        if let mydb = db {
+
+            let tableName: String = "people"
+
+            // test create
+            let _ = mydb.createTable(tableName, columnsInfo: [
                 "id integer primary key autoincrement",
                 "name text",
                 "height double"])
+
+            // test insert
+            let _ = mydb.insert(tableName, rowInfo: ["name":"'Stephen Curry'","height":"1.88"])
+
+            // select
+            let statement = mydb.fetch(tableName, cond: "1 == 1", order: nil)
+
+            // This while loop reads the statement returned by the sqlite db line by line and exits when there are no more lines
+            while sqlite3_step(statement) == SQLITE_ROW {
+                let id = sqlite3_column_int(statement, 0)
+                let name = String(cString: sqlite3_column_text(statement, 1))
+                let height = sqlite3_column_double(statement, 2)
+                print("\(id). \(name) height： \(height)")
+            }
+
+            // **IMPORTANT: release statement pointer when done
+            sqlite3_finalize(statement)
+
+            // update
+            let _ = mydb.update(tableName, cond: "id = 1",
+                rowInfo: ["name":"'LeBron James'","height":"2.06"])
+
+            // delete
+            let _ = mydb.delete(tableName, cond: "id = 2")
+
+            // select
+            let statement1 = mydb.fetch(tableName, cond: "1 == 1", order: nil)
+
+            // This while loop reads the statement returned by the sqlite db line by line and exits when there are no more lines
+            while sqlite3_step(statement1) == SQLITE_ROW {
+                let id = sqlite3_column_int(statement1, 0)
+                let name = String(cString: sqlite3_column_text(statement1, 1))
+                let height = sqlite3_column_double(statement1, 2)
+                print("\(id). \(name) height： \(height)")
+            }
+
+            sqlite3_finalize(statement1)
         }
+
     }
 
 
